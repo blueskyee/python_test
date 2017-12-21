00e0adfa12a28e9f9b9daea23994029d63f5d236
blueskyee <blueskyee9527@gmail.com>
2017-12-22 00:01:12 +0800

    update script

--- py_substring.py
+++ py_substring.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 test_str='revenue_report.sql'
-print(test_str.split(".",1)[0])
-print test_str.split(".",1)[1]
+print('first part substr:{}'.format(test_str.split(".",1)[0]))
+print('second part substr:{}'.format(test_str.split(".",1)[1]))
 print test_str.split(".",1)
 print test_str.split("@",0)
