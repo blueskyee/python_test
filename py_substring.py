1e06ad45be463933163f58861e6e16daeeff9c6f
blueskyee <blueskyee9527@gmail.com>
2017-12-24 21:11:37 +0800

    update script

--- py_substring.py
+++ py_substring.py
@@ -1,7 +1,13 @@
 import pandas as pd
 
 test_str='revenue_report.sql'
+##split string
 print('first part substr:{}'.format(test_str.split(".",1)[0]))
 print('second part substr:{}'.format(test_str.split(".",1)[1]))
 print test_str.split(".",1)
 print test_str.split("@",0)
+
+##reverse string
+print(test_str[::-1]) 
+print(test_str[6::-1])
+    
