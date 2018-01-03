94a9508acf8c16c257f122dc67251004c1d8bcf2
blueskyee <blueskyee9527@gmail.com>
2018-01-03 23:06:33 +0800

    add py_redis.py

---
+++ py_redis.py
@@ -0,0 +1,14 @@
+import redis
+
+def get_redis(host):
+    pool = redis.ConnectionPool(host=host, port=6379, db=0)
+    return redis.Redis(connection_pool=pool)
+
+
+def set_hash_key_value_by_name(redis, name, key, value):
+    return redis.hset(name, key, value)
+
+
+def get_hash_key_value_by_name(redis, name, key):
+    return redis,hget(name, key)
+
