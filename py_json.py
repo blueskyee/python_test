6026abcc271c248348bc839f1abd5194c7e717e4
blueskyee <blueskyee9527@gmail.com>
2018-01-15 16:32:08 +0800

    update py_json.py

--- py_json.py
+++ py_json.py
@@ -2,7 +2,7 @@ import json
 
 code = 200
 #data = json.dumps(["response", {"code":code, "results": {"topic": "topic_name", "valid_dest": "valid_s3_path", "invalid_dest": "invalid_s3_path"}}], separators=(',',':'))
-data = json.dumps({'response':{'code':code, 'results': {'topic': 'topic_name', 'valid_dest': 'valid_s3_path'}}})
+data = json.dumps({'response':{'code':code, 'results': {'topic': 'topic_name', 'valid_dest': 'valid_s3_path'}}}, indent=4, separators=(',', ': '))
 print(data)
 
 with open("json.txt", "w") as outfile:
